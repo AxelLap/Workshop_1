ebb4d35c5deff4f51f6a23a39a1f4759e2c90371
Axel LAPLACE <axellaplace1709@gmail.com>
2024-07-12 13:39:00 +0200

    Button composent created

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,16 +1,19 @@
-
+"use client";
 import { clsx } from "clsx";
 import { cva } from "class-variance-authority";
 
-const buttonVariant = cva()
+const buttonVariant = cva(
+  "inline-flex items-center justify-center rounded-md px-3 py-1.5 ring-indigo-500 ring-offset-2 focus:outline-none focus:ring text-gray-950 h-8 text-sm font-semibold",
+  {
+    variants: {
+      type: {
+        headerBtn: "bg-transparent hover:bg-gray-200 ",
+        cardBtn: "bg-gray-200 hover:bg-gray-300 ml-auto",
+      },
+    },
+  }
+);
 
-// inline-flex items-center justify-center rounded-md px-3 py-1.5 ring-indigo-500 ring-offset-2 focus:outline-none focus:ring bg-transparent text-gray-950 hover:bg-gray-200 h-8 text-sm font-semibold
-export default Button ({children}){
-  return(
-    <button
-
-    >
-      {children}
-    </button>
-  )
-}
+export const Button = ({ type, children }) => {
+  return <button className={buttonVariant({ type: type })}>{children}</button>;
+};
